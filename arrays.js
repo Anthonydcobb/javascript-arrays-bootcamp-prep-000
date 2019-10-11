ab8063be13f58ab047bc0b8dd914780f2c7a05f8
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-11 16:04:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-
-var addElementToBeginningOfArray = (array, element) => {
-   return array.unshift(element);
-
-}
-
-var destructivelyAddElementToBeginningOfArray = (array, element) => {
-  return array[0] = element;
-}
-
-addElementToBeginningOfArray(chocolateBars,)
+var addElementToBeginningOfArray = () =>
